091cc72c3e1044b312772732afad6f6456c4240c
Admin <duckjopa200@gmail.com>
2026-02-24 02:17:58 +0200

    cookie-clicker added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//Menu open&close button
+
 const menuBtn = document.querySelector('#menuBtn')
 const menu = document.querySelector('#menu')
+const cookieBtn = document.querySelector("#cookie")
 
 menuBtn.addEventListener('click', function(){
   menuBtn.classList.toggle('active');
